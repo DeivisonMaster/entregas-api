f4edb833d40915cbd2de0c87266c7bd34ddb5c96
deivisonmaster <deivisonoliveira.info@gmail.com>
2022-09-21 13:27:57 -0400

    config PK de cliente e inserindo registros na base com flyway

--- src/main/java/br/com/entrega/model/Cliente.java
+++ src/main/java/br/com/entrega/model/Cliente.java
@@ -1,25 +1,37 @@
 package br.com.entrega.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 @Entity
 @Table(name = "cliente")
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Size(max = 60)
 	private String nome;
+	
+	@Size(max = 60)
 	private String email;
+	
+	@Size(max = 60)
+	@Column(name = "fone")
 	private String telefone;
 	
 	
---
+++ src/main/resources/db/migration/V003__inclui-cliente.sql
@@ -0,0 +1,2 @@
+insert into cliente (nome, email, fone) values ('João', 'joao@email.com', '98888-5555');
+insert into cliente (nome, email, fone) values ('Maria', 'maria@email.com', '98888-4444');
